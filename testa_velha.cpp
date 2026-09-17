72a71cb08c18e100874f57566ce4500ce6c46fbb
gSperandio <giovanna.sperandio18@gmail.com>
2026-09-16 23:01:20 -0300

    refatoracao final

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -37,57 +37,64 @@ TEST_CASE("Testa velha", "[single-file]") {
   REQUIRE(VerificaVelha(teste2) == 1);  // verifica X vencedor na linha
 
   int teste3[3][3] = {{1, 0, 0}, {2, 1, 0}, {0, 2, 1}};
-  REQUIRE(VerificaVelha(teste3) == 1);  // verifica x vencedor na diagonal principal
+  REQUIRE(VerificaVelha(teste3) ==
+          1);  // verifica x vencedor na diagonal principal
 
-  int teste4[3][3] = {{0,2,1}, {2,1,0}, {1,0,0}};
-  REQUIRE(VerificaVelha(teste4) == 1);  // verifica x vencedor na diagonal secundária
+  int teste4[3][3] = {{0, 2, 1}, {2, 1, 0}, {1, 0, 0}};
+  REQUIRE(VerificaVelha(teste4) ==
+          1);  // verifica x vencedor na diagonal secundária
 
-  //os mesmos 4 testes para vencedor O:
+  // os mesmos 4 testes para vencedor O:
 
-  int teste5[3][3] = {{1,2,1}, {1,2,0}, {0,2,0}};
-  REQUIRE(VerificaVelha(teste5)== 2);  // verifica O vencedor na coluna
+  int teste5[3][3] = {{1, 2, 1}, {1, 2, 0}, {0, 2, 0}};
+  REQUIRE(VerificaVelha(teste5) == 2);  // verifica O vencedor na coluna
 
-  int teste6[3][3] = {{1,1,0}, {2,2,2}, {0,0,1}};
+  int teste6[3][3] = {{1, 1, 0}, {2, 2, 2}, {0, 0, 1}};
   REQUIRE(VerificaVelha(teste6) == 2);  // verifica O vencedor na linha
 
-  int teste7[3][3] = {{2,0,0}, {1,2,0}, {1,1,2}};
-  REQUIRE(VerificaVelha(teste7) == 2);  // verifica O vencedor na diagonal principal
+  int teste7[3][3] = {{2, 0, 0}, {1, 2, 0}, {1, 1, 2}};
+  REQUIRE(VerificaVelha(teste7) ==
+          2);  // verifica O vencedor na diagonal principal
 
-  int teste8[3][3] = {{0,1,2}, {1,2,1}, {2,0,0}};
-  REQUIRE(VerificaVelha(teste8) == 2);  // verifica O vencedor na diagonal secundária
+  int teste8[3][3] = {{0, 1, 2}, {1, 2, 1}, {2, 0, 0}};
+  REQUIRE(VerificaVelha(teste8) ==
+          2);  // verifica O vencedor na diagonal secundária
 
-  //testes de impossibilidade
-  int teste9[3][3] = {{1,1,1},{1,1,1},{1,1,1}};
-  REQUIRE(VerificaVelha(teste9) == -2); //verifica impossibilidade de apenas X
+  // testes de impossibilidade
+  int teste9[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+  REQUIRE(VerificaVelha(teste9) == -2);  // verifica impossibilidade de apenas X
 
-  int teste10[3][3] = {{2,2,2},{2,2,2},{2,2,2}};
-  REQUIRE(VerificaVelha(teste10) == -2); //verifica impossibilidade de apenas O
+  int teste10[3][3] = {{2, 2, 2}, {2, 2, 2}, {2, 2, 2}};
+  REQUIRE(VerificaVelha(teste10) == -2);  // verifica impossibilidade de apenas
+                                          // O
 
-  int teste11[3][3] = {{1,0,1},{1,0,1},{0,2,2}};
-  REQUIRE(VerificaVelha(teste11) == -2); //verifica impossibilidade de pular a vez do X
+  int teste11[3][3] = {{1, 0, 1}, {1, 0, 1}, {0, 2, 2}};
+  REQUIRE(VerificaVelha(teste11) ==
+          -2);  // verifica impossibilidade de pular a vez do X
 
-  int teste12[3][3] = {{1,1,1},{2,2,2},{0,0,0}};
-  REQUIRE(VerificaVelha(teste12) == -2); //verifica impossibilidade de haver 2 vencedores
+  int teste12[3][3] = {{1, 1, 1}, {2, 2, 2}, {0, 0, 0}};
+  REQUIRE(VerificaVelha(teste12) ==
+          -2);  // verifica impossibilidade de haver 2 vencedores
 
-  int teste13[3][3] = {{1,1,1},{1,1,2},{0,0,0}};
-  REQUIRE(VerificaVelha(teste13) == -2); //verifica impossibilidade do O jogar mais que 4 vezes
+  int teste13[3][3] = {{1, 1, 1}, {1, 1, 2}, {0, 0, 0}};
+  REQUIRE(VerificaVelha(teste13) ==
+          -2);  // verifica impossibilidade do O jogar mais que 4 vezes
 
-  int teste14[3][3] = {{2,2,2},{2,2,2},{1,0,1}};
-  REQUIRE(VerificaVelha(teste14) == -2); //verifica impossibilidade do X jogar mais que 5 vezes
+  int teste14[3][3] = {{2, 2, 2}, {2, 2, 2}, {1, 0, 1}};
+  REQUIRE(VerificaVelha(teste14) ==
+          -2);  // verifica impossibilidade do X jogar mais que 5 vezes
 
-  //teste de empate
-  int teste15[3][3] = {{1,2,2},{2,1,1},{1,1,2}};
+  // teste de empate
+  int teste15[3][3] = {{1, 2, 2}, {2, 1, 1}, {1, 1, 2}};
   REQUIRE(VerificaVelha(teste15) == 0);
 
-  //testes de indefinido/inacabado
-  int teste16[3][3] = {{0,2,0}, {0,1,1}, {0,0,0}};
+  // testes de indefinido/inacabado
+  int teste16[3][3] = {{0, 2, 0}, {0, 1, 1}, {0, 0, 0}};
   REQUIRE(VerificaVelha(teste16) == -1);
 
-  int teste17[3][3] = {{0,1,0}, {2,0,0}, {1,0,0}};
+  int teste17[3][3] = {{0, 1, 0}, {2, 0, 0}, {1, 0, 0}};
   REQUIRE(VerificaVelha(teste17) == -1);
 
-  int teste18[3][3] = {{0,0,0}, {1,2,0}, {0,0,0}};
+  int teste18[3][3] = {{0, 0, 0}, {1, 2, 0}, {0, 0, 0}};
   REQUIRE(VerificaVelha(teste18) == -1);
-
 }
-
